c068828572a390cdea5236a03b6f8085d26116fc
Ricardo <ricardo.machado.nwi@gmail.com>
2021-07-23 17:36:51 -0300

    header almost done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { FaTwitter , FaFacebook , FaWhatsapp , FaRegUser } from 'react-icons/fa';
 import { IoIosSearch } from 'react-icons/io';
+import { useState } from 'react';
 import './style.scss';
 import Logo from '../../images/Chippu2.png';
 
+
+
 export function Header(){
+
+    const [ selectedMenu , setSelectedMenu ] = useState<Array<boolean>>(
+        [
+            true,
+            false,
+            false,
+            false,
+            false
+        ]
+    )
+
+    function handleMenuClick(i : number) {
+        const newMenu = selectedMenu.map( (menu ,index) => {
+            if (i === index){
+                return true;
+            } else {
+                return false;
+            }
+        })
+        setSelectedMenu(newMenu)
+    }
 
     return(
         <div className="header-top">
@@ -19,16 +44,37 @@ export function Header(){
 
             <div className="header-dark">
                 <img src={Logo} alt="Chippu Logo" />
-                <nav>
-                    <a href="#">Home</a>
-                    <a href="#">Séries</a>
-                    <a href="#">TV e Séries</a>
-                    <a href="#">Filmes</a>
-                    <a href="#">Filmes</a>
-                    <a href="#">Plataformas</a>
+                <nav className='navigation'>
+                    <a
+                    href="#"
+                    className={ selectedMenu[0] ? 'active' : ''}
+                    onClick={ ()=> handleMenuClick(0) }
+                    >Home</a>
+
+                    <a
+                    href="#"
+                    className={ selectedMenu[1] ? 'active' : ''}
+                    onClick={ ()=> handleMenuClick(1) }
+                    >Séries</a>
+
+                    <a
+                    href="#"
+                    className={ selectedMenu[2] ? 'active' : ''}
+                    onClick={ ()=> handleMenuClick(2) }
+                    >TV e Séries</a>
+
+                    <a href="#"
+                    className={ selectedMenu[3] ? 'active' : ''}
+                    onClick={ ()=> handleMenuClick(3) }
+                    > Filmes</a>
+
+                    <a href="#"
+                    className={ selectedMenu[4] ? 'active' : ''}
+                    onClick={ ()=> handleMenuClick(4) }
+                    >Plataformas</a>
                 </nav>
 
-                <nav>
+                <nav className="stores">
                     <span>Podcast</span>
                     <a href="#">Google</a>
                     <a href="#">Apple</a>
